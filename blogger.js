c18ebf7e1e41cb183b8b6b3646989b118fc87c3f
sabirmostofa <sabirmostofa@gmail.com>
2010-12-03 08:13:24 -0800

    m

--- blogger.js
+++ blogger.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+//first post
 $('#test1').click(function(){
 $('#test11').hide().css('background-color','green').slideToggle('slow');
 });
+//animate post
+$("#test2").click(function(){
+  $("#test222").animate({"left": "+=50px"}, "slow");
+});
+
+$("#test22").click(function(){
+  $("#test222").animate({"left": "-=50px"}, "slow");
+});
+
 });
